dd0862063edf329d16b34c7f41c44bda321cb732
Jess Alejo <tbki.jea@gmail.com>
2023-10-13 11:10:44 +0800

    Use Intention Actions to Insert Wrapper Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,24 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Center(
+          title: const Center(
             child: Text('Dicee'),
           ),
-          backgroundColor: Colors.red,
+          backgroundColor: const Color.fromARGB(255, 218, 0, 55),
         ),
-        body: DicePage(),
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromRGBO(255, 146, 93, 255),
+                  Color.fromARGB(255, 255, 116, 49)
+                ],
+              ),
+            ),
+            child: DicePage()),
       ),
     ),
   );
@@ -20,11 +30,21 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(child: Image.asset("assets/images/dice1.png")),
-        Expanded(child: Image.asset("assets/images/dice1.png")),
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset("assets/images/dice1.png"),
+          )),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset("assets/images/dice1.png"),
+          )),
+        ],
+      ),
     );
   }
 }
